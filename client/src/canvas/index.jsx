1523cefd0939e01c8500f7c450cc826e38d28589
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-02-06 22:02:18 +0200

    Basic Scafolding of pages are added

---
+++ client/src/canvas/index.jsx
@@ -0,0 +1,8 @@
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei'
+
+const CanvasModel = () => {
+	return <h1>Canvas</h1>
+}
+
+export default CanvasModel
