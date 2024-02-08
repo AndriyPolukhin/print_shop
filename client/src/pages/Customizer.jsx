90ff9520cfd42d2fc0c962c457d797f8061648ca
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-02-08 13:04:25 +0200

    Added AIPicker to generate images with openai

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -31,15 +31,88 @@ const Customizer = () => {
 	})
 
 	// Show tab content depending on the activeTab
-	const generateTabContent = () => {}
+	const generateTabContent = () => {
+		switch (activeEditorTab) {
+			case 'colorpicker':
+				return <ColorPicker />
+			case 'filepicker':
+				return <FilePicker file={file} setFile={setFile} readFile={readFile} />
+			case 'aipicker':
+				return (
+					<AIPicker
+						prompt={prompt}
+						setPrompt={setPrompt}
+						generatingImg={generatingImg}
+						handleSubmit={handleSubmit}
+					/>
+				)
+			default:
+				return null
+		}
+	}
 
-	const handleSubmit = async (type) => {}
+	const handleSubmit = async (type) => {
+		if (!prompt) return alert('Please enter a prompt')
 
-	const handleDecals = (type, result) => {}
+		try {
+			setGeneratingImg(true)
 
-	const handleActiveFilterTab = (tabName) => {}
+			const response = await fetch('http://localhost:8080/api/v1/dalle', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ prompt }),
+			})
 
-	const readFile = (type) => {}
+			const data = await response.json()
+
+			handleDecals(type, `data:image/png;base64,${data.photo}`)
+		} catch (error) {
+			alert(error)
+		} finally {
+			setGeneratingImg(false)
+			setActiveEditorTab('')
+			setPrompt('')
+		}
+	}
+
+	const handleDecals = (type, result) => {
+		const decalType = DecalTypes[type]
+		state[decalType.stateProperty] = result
+		if (!activeFilterTab[decalType.filterTab]) {
+			handleActiveFilterTab(decalType.filterTab)
+		}
+	}
+
+	const handleActiveFilterTab = (tabName) => {
+		switch (tabName) {
+			case 'logoShirt':
+				state.isLogoTexture = !activeFilterTab[tabName]
+				break
+			case 'stylishShirt':
+				state.isFullTexture = !activeFilterTab[tabName]
+				break
+			default:
+				state.isLogoTexture = true
+				state.isFullTexture = false
+				break
+		}
+		// after setting the state, activeFilterTab is updated
+		setActiveFilterTab((prevState) => {
+			return {
+				...prevState,
+				[tabName]: !prevState[tabName],
+			}
+		})
+	}
+
+	const readFile = (type) => {
+		reader(file).then((result) => {
+			handleDecals(type, result)
+			setActiveEditorTab('')
+		})
+	}
 
 	return (
 		<AnimatePresence>
@@ -56,9 +129,11 @@ const Customizer = () => {
 									<Tab
 										key={tab.name}
 										tab={tab}
-										handleClick={() => (state.editorTab = tab.name)}
+										handleClick={() => setActiveEditorTab(tab.name)}
 									/>
 								))}
+
+								{generateTabContent()}
 							</div>
 						</div>
 					</motion.div>
